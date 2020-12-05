167c2cc584438ee4576ffb1a77f98c618e1724ab
nabster2010 <nabster201037@gmail.com>
2020-12-06 01:38:16 +0300

    update get ignore

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	MONGODB:
+		'mongodb://nabster2010:nabster2010@cluster0-shard-00-00.k0wfn.mongodb.net:27017,cluster0-shard-00-01.k0wfn.mongodb.net:27017,cluster0-shard-00-02.k0wfn.mongodb.net:27017/social?ssl=true&replicaSet=atlas-wlrzv9-shard-0&authSource=admin&retryWrites=true&w=majority',
+	SECRET_KEY: 'secret key',
+};
